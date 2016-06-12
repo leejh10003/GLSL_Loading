0a55e1aaf11589c65128b48d7a2a751c5832ceeb
LeeJunHyuk <leejh10003@gmail.com>
2016-06-13 02:21:45 +0900

    render of face can be determined

--- main.cpp
+++ main.cpp
@@ -10,15 +10,19 @@
 #include <list>
 #include <vector>
 #include "glsl.h"
+#include <bitset>
 
 using namespace std;
-
 //-----------------------------------------------------------------------------
 typedef enum CoordinateDirection {
 	x = 0b100,
 	y = 0b10,
 	z = 0b1
 }CoordDirec;
+typedef enum BiggerSmaller {
+	smaller = 0,
+	bigger = 1
+}BigSmall;
 struct Vertex
 {
 	GLfloat x;
@@ -68,48 +72,70 @@ class Cube
 {
 protected:
 	Vertex vertices[8];
+	GLfloat r;
+	GLfloat g;
+	GLfloat b;
+	bool rendering[6] = { false, false, false, false, false, false};
 public:
 	void draw()
 	{
 		glBegin(GL_QUADS);
-		glNormal3f(0.0f, 0.0f, 1.0f);
-		glVertex3f(vertices[7].x, vertices[7].y, vertices[7].z);
-		glVertex3f(vertices[5].x, vertices[5].y, vertices[5].z);
-		glVertex3f(vertices[1].x, vertices[1].y, vertices[1].z);
-		glVertex3f(vertices[3].x, vertices[3].y, vertices[3].z);
+		if (rendering[0]) {
+			glNormal3f(0.0f, 0.0f, 1.0f);
+			glColor4f(r, g, b, 1.0f);
+			glVertex3f(vertices[7].x, vertices[7].y, vertices[7].z);
+			glVertex3f(vertices[5].x, vertices[5].y, vertices[5].z);
+			glVertex3f(vertices[1].x, vertices[1].y, vertices[1].z);
+			glVertex3f(vertices[3].x, vertices[3].y, vertices[3].z);
+		}
 
-		glNormal3f(0.0f, 0.0f, -1.0f);
-		glVertex3f(vertices[6].x, vertices[6].y, vertices[6].z);
-		glVertex3f(vertices[4].x, vertices[4].y, vertices[4].z);
-		glVertex3f(vertices[0].x, vertices[0].y, vertices[0].z);
-		glVertex3f(vertices[2].x, vertices[2].y, vertices[2].z);
+		if (rendering[1]) {
+			glNormal3f(0.0f, 0.0f, -1.0f);
+			glColor4f(r, g, b, 1.0f);
+			glVertex3f(vertices[6].x, vertices[6].y, vertices[6].z);
+			glVertex3f(vertices[4].x, vertices[4].y, vertices[4].z);
+			glVertex3f(vertices[0].x, vertices[0].y, vertices[0].z);
+			glVertex3f(vertices[2].x, vertices[2].y, vertices[2].z);
+		}
 
-		glNormal3f(0.0f, 1.0f, 0.0f);
-		glVertex3f(vertices[7].x, vertices[7].y, vertices[7].z);
-		glVertex3f(vertices[6].x, vertices[6].y, vertices[6].z);
-		glVertex3f(vertices[2].x, vertices[2].y, vertices[2].z);
-		glVertex3f(vertices[3].x, vertices[3].y, vertices[3].z);
+		if (rendering[2]) {
+			glNormal3f(0.0f, 1.0f, 0.0f);
+			glColor4f(r, g, b, 1.0f);
+			glVertex3f(vertices[7].x, vertices[7].y, vertices[7].z);
+			glVertex3f(vertices[6].x, vertices[6].y, vertices[6].z);
+			glVertex3f(vertices[2].x, vertices[2].y, vertices[2].z);
+			glVertex3f(vertices[3].x, vertices[3].y, vertices[3].z);
+		}
 
-		glNormal3f(-1.0f, 0.0f, 0.0f);
-		glVertex3f(vertices[3].x, vertices[3].y, vertices[3].z);
-		glVertex3f(vertices[2].x, vertices[2].y, vertices[2].z);
-		glVertex3f(vertices[0].x, vertices[0].y, vertices[0].z);
-		glVertex3f(vertices[1].x, vertices[1].y, vertices[1].z);
+		if (rendering[3]) {
+			glNormal3f(-1.0f, 0.0f, 0.0f);
+			glColor4f(r, g, b, 1.0f);
+			glVertex3f(vertices[3].x, vertices[3].y, vertices[3].z);
+			glVertex3f(vertices[2].x, vertices[2].y, vertices[2].z);
+			glVertex3f(vertices[0].x, vertices[0].y, vertices[0].z);
+			glVertex3f(vertices[1].x, vertices[1].y, vertices[1].z);
+		}
 
-		glNormal3f(1.0f, 0.0f, 0.0f);
-		glVertex3f(vertices[7].x, vertices[7].y, vertices[7].z);
-		glVertex3f(vertices[6].x, vertices[6].y, vertices[6].z);
-		glVertex3f(vertices[4].x, vertices[4].y, vertices[4].z);
-		glVertex3f(vertices[5].x, vertices[5].y, vertices[5].z);
+		if (rendering[4]) {
+			glNormal3f(1.0f, 0.0f, 0.0f);
+			glColor4f(r, g, b, 1.0f);
+			glVertex3f(vertices[7].x, vertices[7].y, vertices[7].z);
+			glVertex3f(vertices[6].x, vertices[6].y, vertices[6].z);
+			glVertex3f(vertices[4].x, vertices[4].y, vertices[4].z);
+			glVertex3f(vertices[5].x, vertices[5].y, vertices[5].z);
+		}
 
-		glNormal3f(0.0f, -1.0f, 0.0f);
-		glVertex3f(vertices[5].x, vertices[5].y, vertices[5].z);
-		glVertex3f(vertices[4].x, vertices[4].y, vertices[4].z);
-		glVertex3f(vertices[0].x, vertices[0].y, vertices[0].z);
-		glVertex3f(vertices[1].x, vertices[1].y, vertices[1].z);
+		if (rendering[5]) {
+			glNormal3f(0.0f, -1.0f, 0.0f);
+			glColor4f(r, g, b, 1.0f);
+			glVertex3f(vertices[5].x, vertices[5].y, vertices[5].z);
+			glVertex3f(vertices[4].x, vertices[4].y, vertices[4].z);
+			glVertex3f(vertices[0].x, vertices[0].y, vertices[0].z);
+			glVertex3f(vertices[1].x, vertices[1].y, vertices[1].z);
+		}
 		glEnd();
 	}
-	Cube(int x, int y, int z)
+	Cube(int x, int y, int z, GLfloat r, GLfloat g, GLfloat b, bool renderOrNot[6]) : r(r), g(g), b(b)
 	{
 		GLfloat xLower = x * 0.2f - 0.1f;
 		GLfloat xUpper = x * 0.2f + 0.1f;
@@ -117,15 +143,13 @@ public:
 		GLfloat yUpper = y * 0.2f + 0.1f;
 		GLfloat zLower = z * 0.2f - 0.1f;
 		GLfloat zUpper = z * 0.2f + 0.1f;
-		for (int i = 0; i < 8; i++) {
-			vertices[i].x = i & CoordDirec::x == 0 ? xLower : xUpper;
-			vertices[i].y = i & CoordDirec::y == 0 ? yLower : yUpper;
-			vertices[i].z = i & CoordDirec::z == 0 ? zLower : zUpper;
+		for (int i = 0; i < 6; i++)
+			rendering[i] = renderOrNot[i];
+		for (char i = 0; i < 8; i++) {
+			vertices[i].x = ((bitset<3>(i) & bitset<3>(CoordDirec::x)) != 0 ? xUpper : xLower);
+			vertices[i].y = ((bitset<3>(i) & bitset<3>(CoordDirec::y)) != 0 ? yUpper : yLower);
+			vertices[i].z = ((bitset<3>(i) & bitset<3>(CoordDirec::z)) != 0 ? zUpper : zLower);
 		}
-	}
-	list<Vertex> getVertices()
-	{
-
 	}
 };
 class Platform
@@ -137,23 +161,27 @@ protected:
 	GLint xEnd;
 	GLint yEnd;
 	GLint zEnd;
+	GLfloat r;
+	GLfloat g;
+	GLfloat b;
 	vector<vector<vector<Cube>>> cubes;
 public:
 	void drawPlatform()
 	{
-		for(int i = xStart; i < xEnd; i++)
-			for(int j = yStart; j < yEnd; j++)
-				for (int k = zStart; k < zEnd; k++){
-					drawCube(i * 0.2f, j * 0.2f, k * 0.2f);
+		for (int i = xStart; i < xEnd; i++)
+			for (int j = yStart; j < yEnd; j++)
+				for (int k = zStart; k < zEnd; k++) {
+					cubes[i - xStart][j - yStart][k - zStart].draw();
 				}
 	}
-	Platform(GLint xStart, GLint xEnd, GLint yStart, GLint yEnd, GLint zStart, GLint zEnd) : xStart(xStart), xEnd(xEnd + 1), yStart(yStart), yEnd(yEnd + 1), zStart(zStart), zEnd(zEnd + 1) {
-		for (int i = 0; i < xEnd - xStart; i++) {
+	Platform(GLint xStart, GLint xEnd, GLint yStart, GLint yEnd, GLint zStart, GLint zEnd, GLfloat r, GLfloat g, GLfloat b, bool renderOrNot[6]) : r(r), g(g), b(b), xStart(xStart), xEnd(xEnd + 1), yStart(yStart), yEnd(yEnd + 1), zStart(zStart), zEnd(zEnd + 1) {
+		for (int i = 0; i < xEnd - xStart + 1; i++) {
 			cubes.push_back(vector<vector<Cube>>());
-			for (int j = 0; j < yEnd - yStart; j++) {
+			for (int j = 0; j < yEnd - yStart + 1; j++) {
 				cubes[i].push_back(vector<Cube>());
-				for (int k = 0; k < zEnd - zStart; k++)
-					cubes[i][j].push_back(Cube(xStart + i, yStart + j, zStart + k));
+				for (int k = 0; k < zEnd - zStart + 1; k++) {
+					cubes[i][j].push_back(Cube(xStart + i, yStart + j, zStart + k, r, g, b, renderOrNot));
+				}
 			}
 		}
 
@@ -161,33 +189,38 @@ public:
 };
 class Body
 {
+protected:
+	GLfloat r;
+	GLfloat g;
+	GLfloat b;
 public:
 	vector<Platform> platforms;
-	Body& addPlatform(vector<int> addPlatform)
+	Body& addPlatform(vector<int> addPlatform, bool renderOrNot[6])
 	{
 		platforms.push_back(Platform(addPlatform[0],
-									 addPlatform[1],
-									 addPlatform[2],
-									 addPlatform[3],
-									 addPlatform[4],
-									 addPlatform[5]));
+			addPlatform[1],
+			addPlatform[2],
+			addPlatform[3],
+			addPlatform[4],
+			addPlatform[5], r, g, b, renderOrNot));
 		return *this;
 	}
-	void drawPlatforms(){
+	void drawPlatforms() {
 		for (int i = 0; i < (int)platforms.size(); ++i)
 			platforms[i].drawPlatform();
 	}
+	Body(GLfloat r, GLfloat g, GLfloat b) : r(r), g(g), b(b) {}
 };
 class myWindow : public cwc::glutWindow
 {
 protected:
-   cwc::glShaderManager SM;
-   cwc::glShader *shader;
-   vector<Body> bodies;
-   GLfloat angle = 0.0f;
-   bool pressed = 0;
+	cwc::glShaderManager SM;
+	cwc::glShader *shader;
+	vector<Body> bodies;
+	GLfloat angle = 0.0f;
+	bool pressed = 0;
 public:
-	myWindow(){}
+	myWindow() {}
 	virtual void OnRender(void)
 	{
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -199,106 +232,22 @@ public:
 		glOrtho(-1.5, 1.5, -1.5, 1.5, -1.5, 1.5);
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
-		if(shader) shader->begin();
+		if (shader) shader->begin();
 		//draw rotating platforms
 		glPushMatrix();
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
 		glRotatef(angle, 0.0f, -1.0f, 0.0f);
-		glColor4f(0.5f, 0.5f, 1.0f, 1.0f);
 		bodies[0].drawPlatforms();
 		glPopMatrix();
 		//draw blue static platforms
 		bodies[1].drawPlatforms();
 		bodies[2].drawPlatforms();
 		//draw red static platforms - where magic goes on!
-		glColor4f(1.0f, 0.5f, 0.5f, 1.0f);
-		for (int i = 4; i < 6; i++) {
-			drawCube(0.0f, 0.4f, i * 0.2f);
-		}
-		{
-			glBegin(GL_QUADS);
-			glNormal3f(0.0f, 0.0f, 1.0f);
-
-			glVertex3f(0.1f + 0.2f * 4, 0.1f - 1.0f, 0.1f);
-			glVertex3f(0.1f + 0.2f * 4, -0.1f - 1.0f, 0.1f);
-			glVertex3f(-0.1f + 0.2f * 4, -0.1f - 1.0f, 0.1f);
-			glVertex3f(-0.1f + 0.2f * 4, 0.1f - 1.0f, 0.1f);
-			/*glNormal3f(0.0f, 0.0f, -1.0f);
-
-			glVertex3f(0.1f + 0.2f * 4, 0.1f - 1.0f, -0.1f);
-			glVertex3f(0.1f + 0.2f * 4, -0.1f - 1.0f, -0.1f);
-			glVertex3f(-0.1f + 0.2f * 4, -0.1f - 1.0f, -0.1f);
-			glVertex3f(-0.1f + 0.2f * 4, 0.1f - 1.0f, -0.1f);*/
-			/*glNormal3f(0.0f, 1.0f, 0.0f);
-
-			glVertex3f(0.1f + 0.2f * 4, 0.1f - 1.0f, 0.1f);
-			glVertex3f(0.1f + 0.2f * 4, 0.1f - 1.0f, -0.1f);
-			glVertex3f(-0.1f + 0.2f * 4, 0.1f - 1.0f, -0.1f);
-			glVertex3f(-0.1f + 0.2f * 4, 0.1f - 1.0f, 0.1f);*/
-			glNormal3f(-1.0f, 0.0f, 0.0f);
-
-			glVertex3f(-0.1f + 0.2f * 4, 0.1f - 1.0f, 0.1f);
-			glVertex3f(-0.1f + 0.2f * 4, 0.1f - 1.0f, -0.1f);
-			glVertex3f(-0.1f + 0.2f * 4, -0.1f - 1.0f, -0.1f);
-			glVertex3f(-0.1f + 0.2f * 4, -0.1f - 1.0f, 0.1f);
-			glNormal3f(1.0f, 0.0f, 0.0f);
-
-			glVertex3f(0.1f + 0.2f * 4, 0.1f - 1.0f, 0.1f);
-			glVertex3f(0.1f + 0.2f * 4, 0.1f - 1.0f, -0.1f);
-			glVertex3f(0.1f + 0.2f * 4, -0.1f - 1.0f, -0.1f);
-			glVertex3f(0.1f + 0.2f * 4, -0.1f - 1.0f, 0.1f);
-			glNormal3f(0.0f, -1.0f, 0.0f);
-
-			glVertex3f(0.1f + 0.2f * 4, -0.1f - 1.0f, 0.1f);
-			glVertex3f(0.1f + 0.2f * 4, -0.1f - 1.0f, -0.1f);
-			glVertex3f(-0.1f + 0.2f * 4, -0.1f - 1.0f, -0.1f);
-			glVertex3f(-0.1f + 0.2f * 4, -0.1f - 1.0f, 0.1f);
-			glEnd();
-		}
-		{
-			glBegin(GL_QUADS);
-			glNormal3f(0.0f, 0.0f, 1.0f);
-			  
-			/*glVertex3f(0.1f - 0.2f, 0.1f + 0.4f, 0.1f + 5 * 0.2f);
-			glVertex3f(0.1f - 0.2f, -0.1f + 0.4f, 0.1f + 5 * 0.2f);
-			glVertex3f(-0.1f - 0.2f, -0.1f + 0.4f, 0.1f + 5 * 0.2f);
-			glVertex3f(-0.1f - 0.2f, 0.1f + 0.4f, 0.1f + 5 * 0.2f);*/
-			/*glNormal3f(0.0f, 0.0f, -1.0f);
-			  
-			glVertex3f(0.1f - 0.2f, 0.1f + 0.4f, -0.1f + 5 * 0.2f);
-			glVertex3f(0.1f - 0.2f, -0.1f + 0.4f, -0.1f + 5 * 0.2f);
-			glVertex3f(-0.1f - 0.2f, -0.1f + 0.4f, -0.1f + 5 * 0.2f);
-			glVertex3f(-0.1f - 0.2f, 0.1f + 0.4f, -0.1f + 5 * 0.2f);*/
-			glNormal3f(0.0f, 1.0f, 0.0f);
-			  
-			glVertex3f(0.1f - 0.2f, 0.1f + 0.4f, 0.1f + 5 * 0.2f);
-			glVertex3f(0.1f - 0.2f, 0.1f + 0.4f, -0.1f + 5 * 0.2f);
-			glVertex3f(-0.1f - 0.2f, 0.1f + 0.4f, -0.1f + 5 * 0.2f);
-			glVertex3f(-0.1f - 0.2f, 0.1f + 0.4f, 0.1f + 5 * 0.2f);
-			/*glNormal3f(-1.0f, 0.0f, 0.0f);
-			  
-			glVertex3f(-0.1f - 0.2f, 0.1f + 0.4f, 0.1f + 5 * 0.2f);
-			glVertex3f(-0.1f - 0.2f, 0.1f + 0.4f, -0.1f + 5 * 0.2f);
-			glVertex3f(-0.1f - 0.2f, -0.1f + 0.4f, -0.1f + 5 * 0.2f);
-			glVertex3f(-0.1f - 0.2f, -0.1f + 0.4f, 0.1f + 5 * 0.2f);*/
-			glNormal3f(1.0f, 0.0f, 0.0f);
-			  
-			glVertex3f(0.1f - 0.2f, 0.1f + 0.4f, 0.1f + 5 * 0.2f);
-			glVertex3f(0.1f - 0.2f, 0.1f + 0.4f, -0.1f + 5 * 0.2f);
-			glVertex3f(0.1f - 0.2f, -0.1f + 0.4f, -0.1f + 5 * 0.2f);
-			glVertex3f(0.1f - 0.2f, -0.1f + 0.4f, 0.1f + 5 * 0.2f);
-			/*glNormal3f(0.0f, -1.0f, 0.0f);
-			  
-			glVertex3f(0.1f - 0.2f, -0.1f + 0.4f, 0.1f + 5 * 0.2f);
-			glVertex3f(0.1f - 0.2f, -0.1f + 0.4f, -0.1f + 5 * 0.2f);
-			glVertex3f(-0.1f - 0.2f, -0.1f + 0.4f, -0.1f + 5 * 0.2f);
-			glVertex3f(-0.1f - 0.2f, -0.1f + 0.4f, 0.1f + 5 * 0.2f);*/
-			glEnd();
-		}
+		bodies[3].drawPlatforms();
 		if (shader) shader->end();
 
-      glutSwapBuffers();
+		glutSwapBuffers();
 
 		Repaint();
 	}
@@ -330,7 +279,7 @@ public:
 		glEnable(GL_LIGHTING);
 		GLfloat lightColor0[] = { 1.0f, 1.0f, 1.0f, 1.0f }; //Color (0.5, 0.5, 0.5)
 		GLfloat lightPos0[] = { 0.0f, 1.6f, 0.0f , 1.0f }; //Positioned at (4, 0, 8)
-		GLfloat ambientColor0[] = {0.6f, 0.6f, 0.6f, 0.1f};
+		GLfloat ambientColor0[] = { 0.6f, 0.6f, 0.6f, 0.1f };
 		GLfloat materialAmbient[] = { .1f, .1f, 0.1f, 1.0f };
 		glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0);
 		glLightfv(GL_LIGHT0, GL_AMBIENT, ambientColor0);
@@ -342,29 +291,36 @@ public:
 		glEnable(GL_LINE_SMOOTH);
 		glEnable(GL_POLYGON_SMOOTH);
 
-		bodies.push_back(Body());
-		bodies.push_back(Body());
-		bodies.push_back(Body());
-		bodies.push_back(Body());
-		bodies[0].addPlatform({ 0, 0, -4, 1, 0, 0 })
-				 .addPlatform({ 0, 3, 2, 2, 0, 0 })
-				 .addPlatform({ 0, 0, -5, -5, -3, 0 });
-		bodies[1].addPlatform({ 0, 0, -5, -5, -7, -4 });
-		bodies[2].addPlatform({ 0, 0, 2, 2, -7, -4 });
-		shader = SM.loadfromFile("vertexshader.txt","fragmentshader.txt"); // load (and compile, link) from file
-		  if (shader==0) 
-			  std::cout << "Error Loading, compiling or linking shader\n";
+		bodies.push_back(Body(0.5f, 0.5f, 1.0f));
+		bodies.push_back(Body(0.5f, 0.5f, 1.0f));
+		bodies.push_back(Body(0.5f, 0.5f, 1.0f));
+		bodies.push_back(Body(1.0f, 0.5f, 0.5f));
+		bool render[6] = { true, true, true, true, true, true };
+		bool notRender[6] = { false, false, false, false, false, false };
+		bool exceptUpperRight[6] = { true, true, false, true, false, true};
+		bool upperRight[6] = { false, false, true, false, true, false };
+		bodies[0].addPlatform({ 0, 0, -4, 1, 0, 0 }, render)
+			.addPlatform({ 0, 3, 2, 2, 0, 0 }, render)
+			.addPlatform({ 0, 0, -5, -5, -3, 0 }, render);
+		bodies[1].addPlatform({ 0, 0, -5, -5, -7, -4 }, render);
+		bodies[2].addPlatform({ 0, 0, 2, 2, -7, -4 }, render);
+		bodies[3].addPlatform({ 0, 0, 2, 2, 4, 5 }, render)
+			.addPlatform({ 4, 4, -5, -5, 0, 0 }, exceptUpperRight)
+			.addPlatform({ -1, -1, 2, 2, 5, 5 }, upperRight);
+		shader = SM.loadfromFile("vertexshader.txt", "fragmentshader.txt"); // load (and compile, link) from file
+		if (shader == 0)
+			std::cout << "Error Loading, compiling or linking shader\n";
 
 	}
 
 	virtual void OnResize(int w, int h) {}
-	virtual void OnClose(void){}
-	virtual void OnMouseDown(int button, int x, int y) {}    
+	virtual void OnClose(void) {}
+	virtual void OnMouseDown(int button, int x, int y) {}
 	virtual void OnMouseUp(int button, int x, int y) {}
-	virtual void OnMouseWheel(int nWheelNumber, int nDirection, int x, int y){}
+	virtual void OnMouseWheel(int nWheelNumber, int nDirection, int x, int y) {}
 
 	virtual void OnKeyDown(int nKey, char cAscii)
-	{       
+	{
 		if (cAscii == 27) // 0x1b = ESC
 		{
 			this->Close(); // Close Window!
@@ -396,7 +352,7 @@ public:
 			for (;angle < 0.0f || angle >= 360.0f;)
 				if (angle >= 360.0f)
 					angle -= 360.0f;
-				else if (angle < 0.0f) 
+				else if (angle < 0.0f)
 					angle += 360.0f;
 			pressed = false;
 		}
@@ -408,7 +364,7 @@ public:
 class myApplication : public cwc::glApplication
 {
 public:
-	virtual void OnInit() {std::cout << "Hello World!\n"; }
+	virtual void OnInit() { std::cout << "Hello World!\n"; }
 };
 
 //-----------------------------------------------------------------------------
